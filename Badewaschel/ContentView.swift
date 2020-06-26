c5d8dc30ae8ac9a02304d185afada18196f99ea3
andreas.hausberger <andreas.hausberger@hpc.at>
2020-06-26 12:27:55 +0200

    Initial Commit

---
+++ Badewaschel/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Badewaschel
+//
+//  Created by Andreas Hausberger on 26.06.20.
+//  Copyright © 2020 Andreas Hausberger. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
